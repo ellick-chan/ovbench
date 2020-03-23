42d6c4663bd7e84904c5e2090716f17e5e57622a
Ellick Chan <ellick.chan@intel.com>
2020-03-22 19:48:55 -0700

    Refactor tools into common tfutil

--- print_graph.py
+++ print_graph.py
@@ -1,25 +1,4 @@
-def load_graph(frozen_graph_filename):
-    import tensorflow as tf
-    with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(f.read())
-    with tf.Graph().as_default() as graph:
-        tf.import_graph_def(graph_def, name="prefix")
-    return graph
-
-def print_graph(model):
-    print('Loading model:', model)
-    graph = load_graph(model)
-    ops = []
-    for op in graph.get_operations(): 
-        print(op.name, op.inputs, ' --> ',  op.outputs)
-        ops.append((op.name, op.inputs, op.outputs))
-
-    print('-'*50)
-    print('First:', ops[0][0], ops[0][2][0].shape)
-    print('Last:', ops[-1][0], ops[-1][2][0].shape)
-
 if __name__ == '__main__':
-    import sys
+    import sys, tfutil
     model = sys.argv[1] if len(sys.argv) >=2 else 'model.pb'
-    print_graph(model)
+    tfutil.print_graph(model)
--- run_tensorflow_pb.py
+++ run_tensorflow_pb.py
@@ -1,26 +1,4 @@
-import tensorflow as tf
-import numpy as np
-
-data        = np.zeros((1, 224, 224, 3))
-input_node  = 'input_1:0'
-output_node = 'fc1000/Softmax:0'
-
-with tf.Session() as sess:
-    options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-    run_metadata = tf.RunMetadata()
-    with open('model.pb', 'rb') as f:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(f.read())
-        _ = tf.import_graph_def(graph_def, name='')
-    
-    input_tensor  = sess.graph.get_tensor_by_name(input_node)
-    output_tensor = sess.graph.get_tensor_by_name(output_node)
-    out = sess.run(output_tensor, feed_dict={input_tensor: data}, options=options, run_metadata=run_metadata)
-    print('Output:', out.shape)
-
-    from tensorflow.python.client import timeline
-    fetched_timeline = timeline.Timeline(run_metadata.step_stats)
-    chrome_trace     = fetched_timeline.generate_chrome_trace_format()
-    with open('timeline.ctf.json', 'w') as f:
-        f.write(chrome_trace)
-        print('Wrote Tensorflow timeline')
+if __name__ == '__main__':
+    import tfutil
+    shp = (1, 224, 224, 3)
+    tfutil.run_tensorflow_pb('model.pb', 'input_1:0', 'fc1000/Softmax:0', shp)
