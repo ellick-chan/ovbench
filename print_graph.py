fbefbddf61e66faf8316adca53be27e71054fe38
Ellick Chan <ellick.chan@intel.com>
2020-03-22 19:37:46 -0700

    Refactor to accept any pb file

--- print_graph.py
+++ print_graph.py
@@ -1,6 +1,5 @@
-import tensorflow as tf
-
 def load_graph(frozen_graph_filename):
+    import tensorflow as tf
     with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
@@ -8,12 +7,19 @@ def load_graph(frozen_graph_filename):
         tf.import_graph_def(graph_def, name="prefix")
     return graph
 
-graph = load_graph('model.pb')
-ops = []
-for op in graph.get_operations(): 
-    print(op.name, op.inputs, ' --> ',  op.outputs)
-    ops.append((op.name, op.inputs, op.outputs))
+def print_graph(model):
+    print('Loading model:', model)
+    graph = load_graph(model)
+    ops = []
+    for op in graph.get_operations(): 
+        print(op.name, op.inputs, ' --> ',  op.outputs)
+        ops.append((op.name, op.inputs, op.outputs))
 
-print('-'*50)
-print('First:', ops[0][0], ops[0][2][0].shape)
-print('Last:', ops[-1][0], ops[-1][2][0].shape)
+    print('-'*50)
+    print('First:', ops[0][0], ops[0][2][0].shape)
+    print('Last:', ops[-1][0], ops[-1][2][0].shape)
+
+if __name__ == '__main__':
+    import sys
+    model = sys.argv[1] if len(sys.argv) >=2 else 'model.pb'
+    print_graph(model)
